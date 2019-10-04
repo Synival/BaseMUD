22526ca3e1d7a6397859d107d1514ffee58cd09c
Simon Bielman <simon@baroque-creations.com>
2019-10-04 23:30:34 +0200

    Removed some files unecessary to releases

--- src/TODO_count.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-count=$(grep "TODO" *.c *.h | wc -l)
-files=$(grep -Hl "TODO" *.c *.h | wc -l)
-
-echo "${count} TODOs in ${files} files."
--- src/TODO_edit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-files=$(grep -l "TODO" *.c *.h)
-# filtered=$(grep -L "PUT INTO TRELLO" $files)
-vim trello.txt $files
